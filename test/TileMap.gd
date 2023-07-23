f696e0d3c503b5d16a7581d5444e9f59b30c8dee
Darr3n2GG <139633310+Darr3n2GG@users.noreply.github.com>
2023-07-23 22:12:04 +0800

    added new assets

--- test/TileMap.gd
+++ test/TileMap.gd
@@ -1,14 +1,14 @@
 extends TileMap
-
-
-var has_mail := false
-
-
-func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		var map_pos = local_to_map(event.position)
-		var tile_id = get_cell_atlas_coords(2, Vector2i(1,0))
-
-		if tile_id == Vector2i(8, 17):
-			has_mail = true
-			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 17))
+#
+#
+#var has_mail := false
+#
+#
+#func _input(event):
+#	if event is InputEventMouseButton and event.is_pressed():
+#		var map_pos = local_to_map(event.position)
+#		var tile_id = get_cell_atlas_coords(2, Vector2i(1,0))
+#
+#		if tile_id == Vector2i(8, 17):
+#			has_mail = true
+#			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 17))
