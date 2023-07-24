aac0366afdd500741f47626d5121fd19b1bb2a48
Darr3n2GG <139633310+Darr3n2GG@users.noreply.github.com>
2023-07-24 19:02:33 +0800

    update place code

--- test/ChangeTiles.gd
+++ test/ChangeTiles.gd
@@ -1,20 +1,38 @@
 extends TileMap
 
-const TILE_SIZE = 16 # Replace this with your actual tile size if different
-
-# Called when the node enters the scene tree for the first time.
+@export var TILE_SIZE = 16 
+@onready var player = $"../Player_Level"
+@export var placed = 0
+@export var dir = 0
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("mb_left"):
-		# Get the current mouse position in local coordinates of the "ChangeTiles" node
-		var global_mouse_pos = get_global_mouse_position()
-		# Convert the local mouse position to the corresponding tile coord
-		var tile_coords = (global_mouse_pos / TILE_SIZE).floor()
-#		print(tile_coords)
-		# Get the tile ID at the mouse position on the specified layer
+		var tile_coords = (get_global_mouse_position() / TILE_SIZE).floor()
+		print(tile_coords)
 		var tile_id = get_cell_source_id(0,Vector2i(tile_coords))
-		# Perform any actions based on the detected tile ID
-#		print("Tile ID at mouse position:", tile_id)
-		if tile_id == -1:
+		var tile_left_id = get_cell_source_id(0,Vector2i(tile_coords.x + 1,tile_coords.y))
+		var tile_right_id = get_cell_source_id(0,Vector2i(tile_coords.x - 1,tile_coords.y))
+		var tile_up_id = get_cell_source_id(0,Vector2i(tile_coords.x,tile_coords.y + 1))
+		var tile_down_id = get_cell_source_id(0,Vector2i(tile_coords.x,tile_coords.y - 1))
+		print("Left Tile ID:", tile_left_id ,
+		"\nRight Tile ID:", tile_right_id, "\nUp Tile ID:", tile_up_id, "\nDown Tile ID:", tile_down_id)
+		if tile_left_id == 1:
+			dir = "right"
+		elif  tile_right_id == 1:
+			dir = "left"
+		elif  tile_up_id == 1:
+			dir = "down"
+		elif  tile_down_id == 1:
+			dir = "up"
+		if tile_left_id == -1 and tile_right_id == -1 and tile_down_id == -1 and tile_up_id == -1:
 			set_cell(0,tile_coords,1 ,Vector2i(0,0))
-		elif tile_id == 1:
+		elif tile_left_id == 1 and dir == "right" or tile_right_id == 1 and dir == "left" or tile_up_id == 1 and dir == "down" or tile_down_id == 1 and dir == "up":
+			print("place is 1")
+			set_cell(0,tile_coords,1 ,Vector2i(0,0))
+		else:
+			pass
+	if Input.is_action_just_pressed("mb_right"):
+		var tile_coords = (get_global_mouse_position() / TILE_SIZE).floor()
+		var tile_id = get_cell_source_id(0,Vector2i(tile_coords))
+		if tile_id == 1:
 			set_cell(0,tile_coords,-1 ,Vector2i(0,0))
+ 
--- test/SelectTiles.gd
+++ test/SelectTiles.gd
@@ -10,11 +10,12 @@ func _physics_process(_delta):
 #	detect_block()
 	
 func update_position_snapped():
-	mouse_pos = Vector2(int(get_global_mouse_position().x / snap_x),int(get_global_mouse_position().y / snap_y))
+	var global_mouse = get_global_mouse_position()
+	mouse_pos = Vector2(int(global_mouse.x / snap_x),int(global_mouse.y / snap_y))
 	global_position = (mouse_pos * snap_x) + Vector2(8,8)
-	if get_global_mouse_position().x + 4 < global_position.x:
+	if global_mouse.x + 4 < global_position.x:
 		global_position.x -= 16
-	if get_global_mouse_position().y + 4 < global_position.y:
+	if global_mouse.y + 4 < global_position.y:
 		global_position.y -= 16
 		
 #func detect_block():
