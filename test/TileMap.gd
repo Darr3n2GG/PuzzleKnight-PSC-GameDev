5c4b4129259a2ea9040be9ff4ce84d30f56b1d63
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-07-26 23:50:21 +0800

    I am stucked
    I didn't found the physics column in order to set hitbox to my tiles. Hitbox is required to prevent player walks on water, and preventing them unlocking the future area.

--- test/TileMap.gd
+++ test/TileMap.gd
@@ -1,14 +1,15 @@
 extends TileMap
-#
-#
-#var has_mail := false
-#
-#
-#func _input(event):
-#	if event is InputEventMouseButton and event.is_pressed():
+
+
+var has_mail := true
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.position.x < 717 and event.position.x > 675 and event.position.y < 406 and event.position.y > 323 :
 #		var map_pos = local_to_map(event.position)
-#		var tile_id = get_cell_atlas_coords(2, Vector2i(1,0))
-#
-#		if tile_id == Vector2i(8, 17):
-#			has_mail = true
-#			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 17))
+			var tile_id = get_cell_atlas_coords(2, Vector2i(1,0))
+
+			if tile_id == Vector2i(8, 16):
+				has_mail = false
+			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 16))
