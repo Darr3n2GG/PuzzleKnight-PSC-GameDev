8dd9fe7e13bf71911dbf594bf3b0f40ffe48c8c2
Darr3n2GG <139633310+Darr3n2GG@users.noreply.github.com>
2023-07-27 23:04:44 +0800

    new level tutorial

--- map_scene/mailed.gd
+++ map_scene/mailed.gd
@@ -9,7 +9,6 @@ func _input(event):
 		if event.position.x < 717 and event.position.x > 675 and event.position.y < 406 and event.position.y > 323 :
 #		var map_pos = local_to_map(event.position)
 			var tile_id = get_cell_atlas_coords(2, Vector2i(1,0))
-
 			if tile_id == Vector2i(8, 16):
 				has_mail = false
 			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 16))
--- test/SelectTiles.gd
+++ test/SelectTiles.gd
@@ -1,9 +1,9 @@
 extends Sprite2D
 
-@export var snap_x = 16
-@export var snap_y = 16
+@export var snap = 16
+@onready var tilemap = $"../../TileMap_Level"
 
-var mouse_pos = Vector2.ZERO
+#var mouse_pos = Vector2.ZERO
 
 func _physics_process(_delta):
 	update_position_snapped()
@@ -11,12 +11,6 @@ func _physics_process(_delta):
 	
 func update_position_snapped():
 	var global_mouse = get_global_mouse_position()
-	mouse_pos = Vector2(int(global_mouse.x / snap_x),int(global_mouse.y / snap_y))
-	global_position = (mouse_pos * snap_x) + Vector2(8,8)
-	if global_mouse.x + 4 < global_position.x:
-		global_position.x -= 16
-	if global_mouse.y + 4 < global_position.y:
-		global_position.y -= 16
-		
-#func detect_block():
-#	get_source_id()
+#	mouse_pos = tilemap.local_to_map(global_mouse)
+	global_position = global_mouse
+
