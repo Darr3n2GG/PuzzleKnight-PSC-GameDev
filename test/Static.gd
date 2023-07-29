5c1e0e8189afce5143378248d090c9df59f8d497
Darr3n2GG <139633310+Darr3n2GG@users.noreply.github.com>
2023-07-29 11:53:46 +0800

    updated tutorial

---
+++ test/Static.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+@export var layer = 1
+var speedoffset = 1
+@onready var player = $"../Level_Tutorial/Player_Level"
+
+func _process(_delta):
+	position = -player.position * layer * speedoffset
+
