239ab399080ce5066ccc373256cd321e06419aba
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-07-29 15:43:27 +0800

    q4rv4e
    hredfbtrhun

---
+++ Stamp.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+
+func _input(event):
+
+	if Input.is_action_pressed("mb_left"):
+		var mouse = get_local_mouse_position()
+#		print(mouse)
+#		382.5, -125, 607.5 105
+		if mouse.x >= 382.5 and mouse.x <= 607.5  and  mouse.y <= 105 and mouse.y >= -125:
+			get_tree().change_scene_to_file("res://platform_levels/level_tutorial.tscn")
+			print("Welcome to tutorial world!")
+			
+			
--- map_scene/Mail.gd
+++ map_scene/Mail.gd
@@ -1,9 +1,11 @@
 extends TileMap
 
 ##var has_mail := true
-#@onready var sceneTree = get_tree()
+@onready var popup = $Window
+
 
 func _input(event):
+
 	if Input.is_action_pressed("mb_left"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_coords = self.local_to_map(mouse_pos)
@@ -11,8 +13,9 @@ func _input(event):
 		
 		if tile_id == Vector2i(8, 16):
 			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 16))
-			get_tree().change_scene_to_file("res://platform_levels/level_tutorial.tscn")
-			print("Welcome to tutorial world!")
+			popup.show()
+			print("Click on the stamp")
+
 #
 #func load_next_scene():
 #	# Load the next scene (replace "res://scenes/next_scene.tscn" with the path to your next scene)
