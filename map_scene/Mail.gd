b8eb75346b918a1a2b9eca9d4d05cec68b3ac7c2
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-07-29 10:34:12 +0800

    MAILLLBOXXXXX
    ewdszygfvuiwehsaiuf8w negrguegf yugw9esfcu gweuihod]qwke7
    g7
    rgrt
    eargHTR4j

--- map_scene/Mail.gd
+++ map_scene/Mail.gd
@@ -4,21 +4,11 @@ extends TileMap
 
 
 func _input(event):
-	if event is InputEventMouseButton:
-#		print (event.position.x, ",", event.position.y)	
+	if Input.is_action_pressed("mb_left"):
+		var mouse_pos = get_global_mouse_position()
+		var tile_coords = self.local_to_map(mouse_pos)
+		var tile_id = get_cell_atlas_coords(2, Vector2i(tile_coords))
 		
-		var read_x = event.position.x
-		
-		var map_x = local_to_map(read_x)
-		var map_y = local_to_map(event.position.y)
-		print(map_x, ",", map_y)
-
-#		var map_pos = local_to_map(event.position)
-#		print(map_pos)
-		
-		if event.position.x < 652 and event.position.x > 679 and event.position.y < 303 and event.position.y > 381 :
-			var tile_id = get_cell_atlas_coords(0, Vector2i(1,0)) 
-			if tile_id == Vector2i(8, 16):
-	#				has_mail = false
-				set_cell(2, Vector2i(1,0), 0, Vector2i(7, 16))
-				print("Succesfully exceuted")
+		if tile_id == Vector2i(8, 16):
+			set_cell(2, Vector2i(1,0), 1, Vector2i(7, 16))
+			print("Succesfully exceuted")
